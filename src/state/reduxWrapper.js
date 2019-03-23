65eb5271cfb0a107cda4eaf55e3efbeb3405111c
Paul O'Shea <poshea48@msn.com>
2019-03-22 17:50:19 -1000

    store window object modification

--- src/state/reduxWrapper.js
+++ src/state/reduxWrapper.js
@@ -11,7 +11,9 @@ let store;
 const initStore = () => {
   const windowGlobal = typeof window !== 'undefined' && window;
   const devtools =
-    process.env.NODE_ENV === 'development' && windowGlobal.devToolsExtension
+    process.env.NODE_ENV === 'development' &&
+    window &&
+    windowGlobal.devToolsExtension
       ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
       : f => f;
