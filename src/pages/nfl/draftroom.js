58388b2f0fe60c715ae4188b53bcc7b9ac75fa4e
Paul O'Shea <poshea48@msn.com>
2019-03-22 19:09:28 -1000

    removed navigate import

--- src/pages/nfl/draftroom.js
+++ src/pages/nfl/draftroom.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, navigate } from 'gatsby';
+import { graphql } from 'gatsby';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import styled from '@emotion/styled';
